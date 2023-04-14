7b882846a743046d6162afe87052907e0982fee3
Arthur <olehno.arturs@gmail.com>
2023-04-14 15:43:59 +0300

    Initial Commit

---
+++ Chili Test Task/Chili_Test_TaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  Chili_Test_TaskApp.swift
+//  Chili Test Task
+//
+//  Created by Артур Олехно on 14/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct Chili_Test_TaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
